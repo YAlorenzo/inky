04dc7c20707f0222c120dd071d01311f01779de9
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-07-24 22:25:45 +0200

    addHomePage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-center text-[45px] text-blue-800'>Hello Inky!</h1>
+      <h1 className='text-center text-[45px] text-blue-800'>Hello Inky! Home page</h1>
     </>
   )
 }
